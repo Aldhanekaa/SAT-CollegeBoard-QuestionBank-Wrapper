2f54d8d6e3d1cada736dd0d34e5d284563bed1ef
Aldhanekaa <aldhanekadev@gmail.com>
2025-08-06 07:21:08 +0700

    hero page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import FooterSection from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <FooterSection />
       </body>
     </html>
   );
