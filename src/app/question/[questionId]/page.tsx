d92859758d0b1e0e8251a60aa1c7d284700d8719
Aldhanekaa <aldhanekadev@gmail.com>
2025-08-10 20:51:11 +0700

    fix fetch issue

--- src/app/question/[questionId]/page.tsx
+++ src/app/question/[questionId]/page.tsx
@@ -1,8 +1,12 @@
+import { SiteHeader } from "@/app/navbar";
+import React from "react";
+
 async function fetchQuestionById(questionId: string) {
   const response = await fetch(
     `${
-      process.env.NEXT_PUBLIC_URL ||
-      process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+      process.env.NEXT_PUBLIC_URL
+        ? process.env.NEXT_PUBLIC_URL
+        : process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
         ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
         : process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     } /api/question-by-id/${questionId}`
@@ -37,5 +41,9 @@ export default async function Page({
 
   console.log("Question data:", JSON.stringify(result.data, null, 2));
 
-  return <div>hey</div>;
+  return (
+    <React.Fragment>
+      <SiteHeader />
+    </React.Fragment>
+  );
 }
