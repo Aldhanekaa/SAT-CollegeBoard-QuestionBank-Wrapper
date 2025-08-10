cb8a9fbfdab1094eb8ca017afc375800cd871627
Aldhanekaa <aldhanekadev@gmail.com>
2025-08-10 20:50:02 +0700

    add process env in rendering page fetching question by Id

--- src/app/question/[questionId]/page.tsx
+++ src/app/question/[questionId]/page.tsx
@@ -1,6 +1,11 @@
 async function fetchQuestionById(questionId: string) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}/api/question-by-id/${questionId}`
+    `${
+      process.env.NEXT_PUBLIC_URL ||
+      process.env.NEXT_PUBLIC_VERCEL_ENV !== "production"
+        ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+        : process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+    } /api/question-by-id/${questionId}`
   );
 
   if (!response.ok) {
